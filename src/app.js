a1b8c8b84cf047218bb76b1e8370acc01f8516db
shaikh mohd <shaikhmohd00009@gmail.com>
2026-01-27 14:36:48 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -2,28 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-    "/user", 
-        (req,res,next) => {
-            console.log("Route user 1");
-            next();
-            res.send("1st Response");
-        },
-        (req,res,next) => {
-            console.log("Route user 2");
-            next();
-            res.send("2nd Response");
-        },
-        (req,res,next) => {
-            console.log("Route user 3");
-            next();
-            res.send("3rd Response");
-        },
-        (req,res,next) => {
-            console.log("Route user 4");
-            res.send("4th Response");
-        }
-);
+const { adminAuth, userAuth } = require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+
+app.get("/user", userAuth, (req,res) => {
+    res.send("User Data Sent");
+});
+
+app.get("/user/login", (req,res) => {
+    res.send("User logged in successfully");
+});
+
+app.get("/admin/getAllData",(req,res) => {
+    res.send("All Data Sent");
+});
+
+app.get("/admin/deleteUser",(req,res) => {
+    res.send("Deleted a user");
+});
 
 app.listen(3000, ()=> {
     console.log("Server is successfully listening on port 3000... ");
