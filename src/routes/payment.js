3eaa0f8f2cd81aaaf8600c0f09248b7c175e772a
shaikh mohd <shaikhmohd00009@gmail.com>
2026-08-06 17:43:31 +0530

    added new payment verify api

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -109,4 +109,12 @@ paymentRouter.post("/payment/webhook", async (req, res) => {
   }
 });
 
+paymentRouter.get("/payment/verify", userAuth, async (req, res) => {
+  const user = req.user;
+  if(user.isPremium){
+    return res.json({isPremium: true})
+  }
+  return res.json({isPremium: false})
+});
+
 module.exports = paymentRouter;
