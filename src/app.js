4e87afe1401667a28fd3c6adef136ebeaf0c0690
shaikh mohd <shaikhmohd00009@gmail.com>
2026-09-22 01:03:53 +0530

    fix cors configuration

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "https://fronted-coherent-two.vercel.app",
-  "https://coherent.me",
-  "https://www.coherent.me",
+  "https://cohorent.me",
+  "https://www.cohorent.me",
 ].filter(Boolean);
 
 app.use(
